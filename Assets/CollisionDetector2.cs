f2ef0da784307c93dc4695c42ff01f0952eb9e83
IreeneGG <91026233+IreeneGG@users.noreply.github.com>
2025-06-16 11:14:39 +0200

    ultimos cambios

--- Assets/CollisionDetector2.cs
+++ Assets/CollisionDetector2.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class CollisionDetector2 : MonoBehaviour
 {
-    [SerializeField] private GameObject block;
-    [SerializeField] private MyAgent agent1;
-    [SerializeField] private JumpAgent agent2;
+    [SerializeField] private GameObject agente; //es el bloque
+    [SerializeField] private MyAgent myagenteRECOMPENSA1;
+    [SerializeField] private JumpAgent jumpagentRECOMPENSA2;
+   
+    [SerializeField] private MyAgent myagenteNORECOMPENSA1;
+    [SerializeField] private JumpAgent jumpagentNORECOMPENSA2;
+ 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject == block)
+        if (collision.gameObject == agente)
         {
-            Debug.Log("GOAL!");
-            agent2.AddReward(100f);
-            agent1.AddReward(50f);
+            Debug.Log("GOAL FINAL!");
+            LogToFile.Write("GOAL FINAL!"); // <== NUEVO
+
+            myagenteRECOMPENSA1.AddReward(100f);
+            jumpagentRECOMPENSA2.AddReward(100f);
+            
+
+            //myagenteNORECOMPENSA1.AddReward(-100f);
+            //myagenteNORECOMPENSA2.AddReward(-100f);
+            //myagenteNORECOMPENSA3.AddReward(-100f);
+
+            //myagenteNORECOMPENSA1.EndEpisode();
+            //myagenteNORECOMPENSA2.EndEpisode();
+            //myagenteNORECOMPENSA3.EndEpisode();
+
+            myagenteRECOMPENSA1.EndEpisode();
+            jumpagentRECOMPENSA2.EndEpisode();
            
-            agent1.EndEpisode();
-            agent2.EndEpisode();
+
+            myagenteNORECOMPENSA1.EndEpisode();
+            jumpagentNORECOMPENSA2.EndEpisode();
+           
+            //jumpagenteRECOMPENSA.AddReward(50f);
+
+            //jumpagenteNORECOMPENSA.EndEpisode();
+            //jumpagenteRECOMPENSA.EndEpisode();
+            //myagenteNORECOMPENSA.EndEpisode();
+            //myagenteRECOMPENSA.EndEpisode();
         }
     }
 }
